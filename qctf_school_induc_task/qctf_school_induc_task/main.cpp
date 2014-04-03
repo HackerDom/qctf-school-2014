6b73f10164894d61036c8a3e76918efc2f55ba84
kopeicev <kopeicev@kasperskyclub.com>
2014-04-03 11:08:20 +0600

    task development finished

--- qctf_school_induc_task/qctf_school_induc_task/main.cpp
+++ qctf_school_induc_task/qctf_school_induc_task/main.cpp
@@ -3,22 +3,26 @@
 
 int main()
 {
-	char str[] = "SQCTF:no8fdX9qeA55";
-	int len = strlen(str);
+	char* buf = (char*)malloc(100);
+	memset(buf, 0, 100);
+	printf("Hello, this is simple crypter\n");
+	printf("Write data for crypt: ");
+	scanf_s("%s", buf, 100);
+	int len = strlen(buf);
 	
 	for (int i = 0; i < len; i++)
 	{
-		if (str[i] > 0x60)
-			str[i] += 5;
+		if (buf[i] > 0x60)
+			buf[i] += 5;
 		
-		if ((str[i] > 0x2f) && (str[i] < 0x3a))
-			str[i] -= 7;
+		if ((buf[i] > 0x2f) && (buf[i] < 0x3a))
+			buf[i] -= 7;
 
-		str[i] ^= 5;
+		buf[i] ^= 5;
 	}
 
-	
-	printf("%s", str);
+	printf("%s", buf);
+	delete [] buf;
 
 	return 0;
 }
