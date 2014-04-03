dcf6be5f35e9fd99bc9b36339cb727d0efb5f7c2
m_messiah <m.muzafarov@gmail.com>
2014-04-03 14:38:21 +0600

    Chrome only
    Работает только в Chrome, зато именно так как подразумевалось.
    Осталось придумать как они должны получить название искомой функции.

--- web_console_task/generic.js
+++ web_console_task/generic.js
@@ -1,35 +1,39 @@
-Object.defineProperty(window, "console", {
-    value: console,
-    writable: false,
-    configurable: false
-});
-
-var i = 0;
-function showWarningAndThrow() {
-    if (!i) {
-        setTimeout(function () {
-            console.log("%cCan't touch this", "color: black; background-color: yellow;");
-        }, 1);
-        i = 1;
-    }
-    throw "Console is disabled";
-}
-
 function help() {
-    console.log("All disabled, except me");
+    console.log("There are few functions whitch allowed to you. Catch 'em all!");
 }
 
-var l, n = {
-        set: function (o) {
-            l = o;
-        },
-        get: function () {
-            //showWarningAndThrow();
-            //console.log(arguments.identity) 
-            throw arguments.callee.name;
-            //return l
-        }
-    };
-Object.defineProperty(console, "_commandLineAPI", n);
-Object.defineProperty(console, "__commandLineAPI", n);
+function pokemon() {
+    console.info("It is right, dude!");
+    //window.location.replace("http://google.ru");
+}
 
+funcs = ["help", "alert", "pokemon"];
+if (window.webkitURL) {
+    var ish, _call = Function.prototype.call;
+    Function.prototype.call = function () {
+        if (arguments.length > 0 && this.name === "evaluate" && arguments[0].constructor.name === "InjectedScriptHost") {
+            ish = arguments[0];
+            ish.evaluate = function (e) {
+                var exists = false;
+                var func = "";
+                for (key in funcs){
+                    if (String(parseInt(key, 10)) === key && funcs.hasOwnProperty(key)) {
+                        if (e.indexOf(funcs[key]) !== -1) {
+                            exists = true;
+                            func = e.slice(e.indexOf(funcs[key]), -2)
+                            f (func.indexOf(';') !== -1) {
+                                func = func.slice(0, func.indexOf(';') + 1);
+                            }
+                            break;
+                        }
+                    }
+                }
+                if (!exists) {throw new Error ('Permission denied!');}
+
+                eval(func);
+            };
+            Function.prototype.call = _call;
+            return _call.apply(this, arguments);
+        }
+    }
+}
