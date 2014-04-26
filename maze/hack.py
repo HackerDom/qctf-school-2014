3a29d5f71f0d7eb53425e8d5f87390959f831178
kopeicev <kopeicev@kasperskyclub.com>
2014-04-26 08:11:19 +0600

    part of qitz and maze implemented

---
+++ maze/hack.py
@@ -0,0 +1,15 @@
+from urllib import request
+from re import findall
+
+
+data = "start"
+while True:
+    handle = request.Request("http://127.0.0.1:5000/" + data, headers = {"user-agent" : "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36"})
+    handle = request.urlopen(handle)
+    rowdata = handle.read().decode('utf-8', errors='ignore')
+    link = findall("href=(.*?) c", rowdata)
+    if len(link) > 0:
+        data = link[0]
+    else:
+        print(rowdata)
+        break
---
+++ maze/serv.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from hashlib import md5
+from flask import request
+
+
+app = Flask(__name__)
+
+@app.route("/<datastr>")
+def hello(datastr):
+    arr = [chr(x+1) for x in list(datastr.encode('utf-8'))]
+    gethash = md5()
+    gethash.update("".join(arr).replace(":", "9").encode('utf-8'))
+    hash = gethash.hexdigest()
+
+    if "Python" in request.headers.get('User-Agent'):
+        return ""
+
+    if not datastr == "57f0eb9a206a640c97ce739791c75ff3":
+        return "<a href=" + "".join(hash) + " class=\"design\">Ссылка</a>"
+    else:
+        return "Your flag is: QCTF_2vlSKFfdYdXWojf"
+
+if __name__ == "__main__":
+    app.run()
